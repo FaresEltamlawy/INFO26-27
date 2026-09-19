ea48affb52b44d61df155bb781644fadda07837a
FaresEltamlawy <fareseltam@gmail.com>
2026-09-19 09:43:04 +0200

    Creazione di un nuovo progetto

--- Esercizijava/Studenti/src/Alunno.java
+++ Esercizijava/Studenti/src/Alunno.java
@@ -4,4 +4,36 @@ public class Alunno {
     int eta;
     int anno;
 
+    public Alunno(int eta, String nome, int anno) {
+        this.eta = eta;
+        this.nome = nome;
+        this.anno = anno;
+    }
+
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+
 }
