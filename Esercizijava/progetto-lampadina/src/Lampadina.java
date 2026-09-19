83b94daf94f65ab8320d6593760830308a4d53ec
FaresEltamlawy <fareseltam@gmail.com>
2026-09-19 09:17:34 +0200

    Creazione della classe Lampadina

---
+++ Esercizijava/progetto-lampadina/src/Lampadina.java
@@ -0,0 +1,59 @@
+public class Lampadina {
+
+    int potenza;
+    String colore;
+    int intensita;
+    boolean accesa;
+    String nome;
+
+    public Lampadina() {
+        this.potenza = potenza;
+        this.colore = colore;
+        this.intensita = intensita;
+        this.accesa = accesa;
+        this.nome = nome;
+    }
+
+    public int getPotenza() {
+        return potenza;
+    }
+
+    public void setPotenza(int potenza) {
+        this.potenza = potenza;
+    }
+
+    public String getColore() {
+        return colore;
+    }
+
+    public void setColore(String colore) {
+        this.colore = colore;
+    }
+
+    public int getIntensita() {
+        return intensita;
+    }
+
+    public void setIntensita(int intensita) {
+        this.intensita = intensita;
+    }
+
+    public boolean isAccesa() {
+        return accesa;
+    }
+
+    public void setAccesa(boolean accesa) {
+        this.accesa = accesa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+}
--- Esercizijava/progetto-lampadina/src/Main.java
+++ Esercizijava/progetto-lampadina/src/Main.java
@@ -3,8 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Wuooooooooooooooo");
-
-
+        Lampadina l;
+        l = new Lampadina();
+        l.potenza = 30;
     }
 }
