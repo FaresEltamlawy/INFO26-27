fd08f3cd0308413a8ef1eb44ab729fd4a05d2b99
FaresEltamlawy <fareseltam@gmail.com>
2026-09-26 09:11:37 +0200

    Creazione di un nuovo progetto

--- Esercizijava/Studenti/src/Alunno.java
+++ Esercizijava/Studenti/src/Alunno.java
@@ -1,39 +1,88 @@
 public class Alunno {
 
-    String nome;
-    int eta;
-    int anno;
+    private String nome;
+    private String cognome;
+    private int eta;
+    private float altezza;
+    private float peso;
 
-    public Alunno(int eta, String nome, int anno) {
-        this.eta = eta;
-        this.nome = nome;
-        this.anno = anno;
+
+    public Alunno() {
+        this.nome = "";
+        this.cognome = "";
+        this.eta = 0;
+        this.altezza = 0;
+        this.peso = 0;
     }
 
 
-    public int getEta() {
-        return eta;
+    public Alunno(Alunno a) {
+        if (a.eta < 5 || a.altezza <= 0 || a.peso <= 0) {
+            System.out.println("cambia i valori che hai messo.");
+            this.nome = "";
+            this.cognome = "";
+            this.eta = 0;
+            this.altezza = 0;
+            this.peso = 0;
+        } else {
+            this.nome = a.nome;
+            this.cognome = a.cognome;
+            this.eta = a.eta;
+            this.altezza = a.altezza;
+            this.peso = a.peso;
+        }
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
     }
 
     public void setEta(int eta) {
         this.eta = eta;
     }
 
-    public String getNome() {
-        return nome;
+    public void setAltezza(float altezza) {
+        this.altezza = altezza;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getAnno() {
-        return anno;
-    }
-
-    public void setAnno(int anno) {
-        this.anno = anno;
+    public void setPeso(float peso) {
+        this.peso = peso;
     }
 
 
+    public String calcolaIndice() {
+        if (altezza <= 0 || peso <= 0) {
+            return "Impossibile calcolare il BMI: valori sbagliati.";
+        }
+
+        double bmi = (peso / (altezza * altezza));
+        String messaggio;
+
+        if (bmi < 18.5) {
+            messaggio = "Sottopeso";
+        } else if (bmi < 25) {
+            messaggio = "Normopeso";
+        } else if (bmi < 30) {
+            messaggio = "Sovrappeso";
+        } else {
+            messaggio = "Obesità";
+        }
+
+        return  "eta:" + eta + " " + "bmi:"+ bmi + "  " + messaggio;
+    }
+
+
+    public void stampa() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Cognome: " + cognome);
+        System.out.println("Età: " + eta);
+        System.out.println("Altezza: " + altezza + " m");
+        System.out.println("Peso: " + peso + " kg");
+
+    }
 }
--- Esercizijava/Studenti/src/Main.java
+++ Esercizijava/Studenti/src/Main.java
@@ -1,9 +1,76 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Scanner input = new Scanner(System.in);
 
+        Alunno a1 = new Alunno();
+        Alunno a2 = new Alunno();
+
+        int scelta;
+
+        do {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1) Inserire i dati dei due studenti");
+            System.out.println("2) Determinare l'indice di massa corporea");
+            System.out.println("3) Stampare le caratteristiche dei due studenti");
+            System.out.println("0) Esci");
+            System.out.print("Scelta: ");
+            scelta = input.nextInt();
+            input.nextLine();
+
+            switch (scelta) {
+                case 1:
+                    System.out.println("\n--- Studente 1 ---");
+                    System.out.print("Nome: ");
+                    a1.setNome(input.nextLine());
+                    System.out.print("Cognome: ");
+                    a1.setCognome(input.nextLine());
+                    System.out.print("Età: ");
+                    a1.setEta(input.nextInt());
+                    System.out.print("Altezza (m): ");
+                    a1.setAltezza(input.nextFloat());
+                    System.out.print("Peso (kg): ");
+                    a1.setPeso(input.nextFloat());
+                    input.nextLine();
+
+                    System.out.println("\n--- Studente 2 ---");
+                    System.out.print("Nome: ");
+                    a2.setNome(input.nextLine());
+                    System.out.print("Cognome: ");
+                    a2.setCognome(input.nextLine());
+                    System.out.print("Età: ");
+                    a2.setEta(input.nextInt());
+                    System.out.print("Altezza (m): ");
+                    a2.setAltezza(input.nextFloat());
+                    System.out.print("Peso (kg): ");
+                    a2.setPeso(input.nextFloat());
+                    input.nextLine();
+                    break;
+
+                case 2:
+                    System.out.println("\nBMI Studente 1: " + a1.calcolaIndice());
+                    System.out.println("BMI Studente 2: " + a2.calcolaIndice());
+                    break;
+
+                case 3:
+                    System.out.println("\n--- Studente 1 ---");
+                    a1.stampa();
+                    System.out.println("\n--- Studente 2 ---");
+                    a2.stampa();
+                    break;
+
+                case 0:
+                    System.out.println("Exit");
+                    break;
+
+                default:
+                    System.out.println("Scelta non valida!");
+            }
+
+        } while (scelta != 0);
+
+        input.close();
     }
 }
